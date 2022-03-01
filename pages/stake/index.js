175f1711646f9ad19b45f3a26fd3a6406f65c4f5
TheGrandNobody <thegrandnobody@protonmail.com>
2022-03-01 18:29:11 +0100

    Fix identation

--- pages/stake/index.js
+++ pages/stake/index.js
@@ -3,40 +3,44 @@ import HEAD from 'next/head';
 import Navbar from '../../components/navbar';
 import Footer from '../../components/Footer/Footer';
 import StakeUI from '../../components/StakeUI/Stake';
-import Web3 from 'web3';
-import { toast } from 'react-toastify';
-import { createUserApprovalStatus } from '../../services';
+import useEternalHook from '../../hooks/useEternalHook';
 
 function index() {
 
-    const handleClickOnApproveBtn = async (amount) => {
-        const approvalreq = await eternalTokenContract.approve('0x2fDA645542F0495a30312A49e5804Efb91409544', Web3.utils.toWei(`${amount}`, 'ether'));
-        let interval = setInterval(async () => {
-          let reciept = await getWeb3NoAccount().eth.getTransactionReceipt(approvalreq.hash);
-          if (reciept) {
-            dispatch(changeApproval({ approval: true }));
-            await createUserApprovalStatus(account, true);
-            toast.success('Approved Successful !', { toastId: 2 });
-            clearInterval(interval);
-          }
-        }, 500);
-    };
+  const {
+    amount,
+    asset,
+    handleOnAmountSelect,
+    handleOnAssetSelect,
+    handleConversionToETRNL,
+    handlePercents,
+    handleClickOnConfirmBtn,
+    handleClickOnApproveBtn,
+    handleUserApproval
+  } = useEternalHook();
 
-    return (
-        <>
-            <HEAD>
-                <title>Stake | Eternal</title>
-            </HEAD>
+  useEffect(() => {
+    (async () => {
+      const approved = await handleUserApproval('treasury');
+      dispatch(changeApproval({ approval: approved }));
+    })();
+  }, [account, asset, amount]);
 
-            <body className='secondary select-deposit-pg'>
-                <div className='header d-flex align-items-center'>
-                    <Navbar />
-                    <StakeUI handleClickOnApproveBtn={handleClickOnApproveBtn} />
-                </div>
-                <Footer />
-            </body>
-        </>
-    );
+  return (
+    <>
+      <HEAD>
+        <title>Stake | Eternal</title>
+      </HEAD>
+
+      <body className='secondary select-deposit-pg'>
+        <div className='header d-flex align-items-center'>
+          <Navbar />
+          <StakeUI handleClickOnApproveBtn={handleClickOnApproveBtn} />
+        </div>
+        <Footer />
+      </body>
+    </>
+  );
 }
 
 export default index;
