9a053d85c15350bdebc41bbf378c3fd7a72d248e
TheGrandNobody <thegrandnobody@protonmail.com>
2022-04-20 12:13:53 +0200

    Update for deployed contracts

--- helpers/addressHelper.js
+++ helpers/addressHelper.js
@@ -1,7 +1,12 @@
 import { addresses, abis } from '../constant/constants';
 
 export const getAddress = (entity) => {
-  return addresses[entity];
+  if (window.ethereum && window.ethereum.on) {
+    const web3 = new Web3(window.ethereum);
+    const id = await web3.eth.net.getId();
+    return addresses[entity][id];
+  }
+  return addresses[entity][43114];
 };
 
 export const getABI = (entity) => {
