f64d5a581ac57f58be7437be2942be94601439b9
TheGrandNobody <thegrandnobody@protonmail.com>
2022-05-02 12:26:04 +0200

    Fix await issue

--- helpers/addressHelper.js
+++ helpers/addressHelper.js
@@ -1,6 +1,7 @@
 import { addresses, abis } from '../constant/constants';
+import Web3 from 'web3';
 
-export const getAddress = (entity) => {
+export const getAddress = async (entity) => {
   if (window.ethereum && window.ethereum.on) {
     const web3 = new Web3(window.ethereum);
     const id = await web3.eth.net.getId();
